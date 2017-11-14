d035eab5caa1322e939710d70f4e531227e5655b
Maksim Kostromin <daggerok@gmail.com>
2017-11-14 23:55:16 +0200

    Initial commit

---
+++ src/stop_any.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# helper stop_any function. usage: `stop_any 8080`
+function stop_any {
+  export port=$1
+  sudo kill $(sudo lsof -t -i:${port}) | true
+}
---
+++ src/wait_for.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# helper wait_for function, usage: `wait_for 8080`
+function wait_for {
+  export port=$1
+  while [ "$(sudo lsof -t -i:${port} | wc -l)" == "0" ]; do sleep 1; done;
+}
