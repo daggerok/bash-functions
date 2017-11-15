b26875cb83f085a673587d644f41b9fbb4ebb4f0
Maksim Kostromin <daggerok@gmail.com>
2017-11-15 03:18:41 +0200

    Update stop_any.bash

--- src/stop_any.bash
+++ src/stop_any.bash
@@ -2,12 +2,12 @@
 
 # helper stop_any function. usage: `stop_any 8080`
 function stop_any {
-  export port=$1
-  # lsof -i tcp:${port} | awk 'NR!=1 {print $2}' | xargs kill | true
-  sudo kill $(sudo lsof -t -i:${port}) > /dev/null 2>$1 | true
-  if [ $? -eq 0 ]; then
-    echo "$port stopped."
-  else
-    echo "Nothing is running on port $port.";
-  fi;
+  for port i $*; do
+    sudo kill $(sudo lsof -t -i:${port}) > /dev/null 2>$1 | true
+    if [ $? -eq 0 ]; then
+      echo "$port stopped."
+    else
+      echo "Nothing is running on port $port.";
+    fi;
+  done
 }
