9b97beb8503aa2c14818a9ed106bc9b792b71139
Maksim Kostromin <daggerok@gmail.com>
2017-11-15 03:22:45 +0200

    Update wait_for.bash

--- src/wait_for.bash
+++ src/wait_for.bash
@@ -2,7 +2,8 @@
 
 # helper wait_for function, usage: `wait_for 8080`
 function wait_for {
-  export port=$1
-  while [ "$(sudo lsof -t -i:${port} | wc -l)" == "0" ]; do sleep 1; done;
-  echo "$port is ready."
+  for port in $*; do
+    while [ "$(sudo lsof -t -i:${port} | wc -l)" == "0" ]; do sleep 1; done;
+    echo "$port is ready."
+  done
 }
