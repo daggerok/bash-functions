764ad7946ca6ea587a80ac8435eedd923fd8435e
Maksim Kostromin <daggerok@gmail.com>
2018-06-13 23:37:45 +0200

    Update wait_for.bash

--- src/wait_for.bash
+++ src/wait_for.bash
@@ -3,7 +3,7 @@
 # helper wait_for function, usage: `wait_for 8080 8081`
 function wait_for {
   for port in "$*"; do
-    if [ "${port}" == "" ]; then continue; fi
+    if [ ".${port}" == "." ]; then continue; fi
     while [ "$(lsof -t -i:${port} | wc -l)" == "0" ]; do sleep 1; done;
     echo "${port} is ready."
     echo "found according PIDs:"
